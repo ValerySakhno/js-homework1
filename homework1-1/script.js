82a8305bde73227a60427392990e2ccdac186176
ValerySakhno <valerius94@inbox.ru>
2020-01-21 19:05:43 +0300

    new commit

--- homework1-1/script.js
+++ homework1-1/script.js
@@ -11,23 +11,8 @@ function button2(){
 }
 
 function button3(){
-    var butt = document.querySelector("butt")
-    butt.innerHTML = "<section id=block></section>"
+    var butt1 = document.getElementsByClassName("butt")
+    for(butt1 of butt1){
+        butt1.classList.add("fade")
+    }
 }
-
-var section = document.getElementById("block");
-for(var i=1; i <= 6; i++){
-block.innerHTML += "<article> + 'Новая кнопка' + i + </article>";
-}
-section.style.display = "flex";
-section.style.justifyContent = "center";
-
-
-var article = document.querySelectorAll("article");
-for(var article of article){
-    article.style.margin = "10px";
-    article.style.background = "#265845";
-}
-
-
-
